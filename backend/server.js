6ca6544ca8b1752bd5f25801599b904d6a5b5030
gabrielzianialves <gabriel.ziani.alves@outlook.com>
2025-12-01 15:03:23 -0300

    alteracoes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const cors = require('cors'); // <--- 1. Importa o módulo CORS
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
-// 2. Configuração do CORS: Permite APENAS a origem do frontend (3000)
+
 const corsOptions = {
     origin: 'http://localhost:3000',
     methods: 'GET',
-    optionsSuccessStatus: 200 // Algumas versões legadas precisam disso
+    optionsSuccessStatus: 200
 };
 
-// 3. Aplica o middleware CORS
+
 app.use(cors(corsOptions));
 
-// Rota simples que o frontend tentará acessar
+
 app.get('/api/mensagem', (req, res) => {
-    // AGORA o navegador permite a comunicação, e a API responde
     res.json({ data: "Mensagem secreta da API em 8080 — COM PERMISSÃO CORS!" });
 });
 
